b4544c2d9078073de0815c228529c784f4fd1b91
Daniel Lee <danieljldev@gmail.com>
2026-07-18 19:16:56 -0700

    Initial commit of OnLife project using Swift

---
+++ OnLife/OnLifeApp.swift
@@ -0,0 +1,32 @@
+//
+//  OnLifeApp.swift
+//  OnLife
+//
+//  Created by Daniel Lee on 7/18/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct OnLifeApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ OnLifeTests/OnLifeTests.swift
@@ -0,0 +1,16 @@
+//
+//  OnLifeTests.swift
+//  OnLifeTests
+//
+//  Created by Daniel Lee on 7/18/26.
+//
+
+import Testing
+
+struct OnLifeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
